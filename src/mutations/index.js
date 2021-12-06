71acc033298c95410b03169bf4f4200001b06b79
Jon Naske <jonnaske@hotmail.com>
2021-12-06 15:55:51 -0800

    implemeted adding book functionality

--- src/mutations/index.js
+++ src/mutations/index.js
@@ -1,14 +1,17 @@
 import { gql } from '@apollo/client';
 
 export const ADD_BOOK = gql`
-  mutation addBook($title: String!, $author: String!, $published: Int!, $genres: [String!]!) {
-    addBook(title: $title, author: $author, published: $published, genres: $genres) {
-      title
-      author {
-        name
-      }
-      published
-      genres
+  mutation addBook($title: String!, $author: String!, $published: Int!, $genres: [String]) {
+    addBook(
+      title: $title, 
+      author: $author, 
+      published: $published, 
+      genres: $genres
+      ) {
+        title
+        author 
+        published
+        genres
     }
   }
 `;
