722c1ab337287174e644e77cb01b91621d3b9ebb
Jon Naske <jonnaske@hotmail.com>
2021-12-31 17:58:03 -0800

    added login form skeleton to app

--- src/App.js
+++ src/App.js
@@ -3,10 +3,15 @@ import React, { useState } from 'react'
 import Authors from './components/Authors'
 import Books from './components/Books'
 import NewBook from './components/NewBook'
+import LoginForm from './components/LoginForm'
 
 const App = () => {
   const [page, setPage] = useState('authors')
+  const [token, setToken] = useState(null)
 
+  if (!token) {
+    return (<LoginForm setToken={setToken} token={token}/>)
+  }
   return (
     <div>
       <div>
@@ -17,14 +22,23 @@ const App = () => {
 
       <Authors
         show={page === 'authors'}
+        token={token}
+        setToken={setToken}
       />
 
       <Books
         show={page === 'books'}
+        token={token}
       />
 
       <NewBook
         show={page === 'add'}
+        token={token}
+      />
+      <LoginForm
+        show={page === 'login'}
+        setToken={setToken}
+        token={token}
       />
 
     </div>
